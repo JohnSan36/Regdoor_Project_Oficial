3f129343969c2ee9045e86a1477602139e513ed8
JohnSan36 <joao0206.jo@gmail.com>
2025-02-24 09:32:23 -0300

    Conectado ao Evolution

--- oficial_version/app.py
+++ oficial_version/app.py
@@ -1,40 +1,23 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import requests
-from dotenv import load_dotenv, find_dotenv
-import uvicorn
+from flask import Flask, request, jsonify
 import os
-
+import json
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 EVOLUTION_API_KEY = os.getenv("EVOLUTION_API_KEY")
 
-app = FastAPI()
+app = Flask(__name__)
 
-class WhatsappMessage(BaseModel):
-    message: str
-    type_message: str
-    sender: str
-
-@app.post("/webhook")
-async def recieve_message(msg: WhatsappMessage):
-    print(f"Nova mensagem de {msg.sender}; Conteúdo: {msg.message}; Type: {msg.type_message}")
-    return {"Status": "Mensagem recebida com sucesso!"}
-
-def send_message(to, message):
-    url = "https://evolution.cybermindsolutions.com.br/api/v1/message/sendText"
-    headers = {"Authorization": f"Bearer {EVOLUTION_API_KEY}"}
-    payload = {"to": to, "message": message}
-
-    response = requests.post(url, headers=headers, json=payload)
-    return response.json()
-
-
-import asyncio
+@app.route("/webhook", methods=["POST"])
+def receive_message():
+    try:
+        body_corp = request.data  
+        body_str = body_corp.decode("utf-8")
+        print("Mensagem recebida:", body_str)  
+        return jsonify({"Status": "Mensagem recebida com sucesso!"})
+    
+    except Exception as e:
+        return jsonify({"Erro": f"Falha ao processar JSON: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    if asyncio.get_event_loop().is_running():
-        print("O servidor já está rodando dentro de um loop assíncrono. Execute diretamente com 'uvicorn app:app --reload'")
-    else:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- oficial_version/main.py
+++ oficial_version/main.py
@@ -1,61 +1,141 @@
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
-from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain.agents import AgentExecutor
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+from langchain.schema.agent import AgentFinish
+from langchain_core.tools import StructuredTool
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain_core.utils.function_calling import convert_to_openai_function
-from langchain_core.output_parsers import StrOutputParser
-from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain.agents.format_scratchpad import format_to_openai_function_messages
+from pydantic import BaseModel, Field
+from langchain.tools import Tool
+from flask import Flask, request, jsonify
+import os
+import json
 from dotenv import load_dotenv, find_dotenv
-
-
-#Importando o modelo de chat Openai e suas credenciais
+from datetime import datetime
 load_dotenv(find_dotenv())
-chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.2)
 
 
-#Trecho usado para exemplo
-texto = ("""Olá, tive uma reunião mais cedo hoje com representantes tanto da Autoridade Europeia dos Valores Mobiliários e dos 
+chat = ChatOpenAI(model="gpt-4o-mini")
+EVOLUTION_API_KEY = os.getenv("EVOLUTION_API_KEY")
+
+
+def obter_hora_e_data_atual():
+    """Retorna a hora atual e a data de hoje."""
+    agora = datetime.now()
+    return agora.strftime("%d-%m-%Y - %H:%M:%S")
+
+
+data_atual = obter_hora_e_data_atual()
+texto = """Olá, tive uma reunião mais cedo hoje com representantes tanto da Autoridade Europeia dos Valores Mobiliários e dos 
 Mercados (ESMA) quanto da Autoridade de Supervisão Financeira da Suécia (FSA). Discutimos a harmonização das 
 regulamentações de ativos digitais em jurisdições da UE e os desafios específicos na implementação de estruturas de 
-conformidade.""")
+conformidade."""
 
 
-#Criando a tool responsavel por extrair as informações do texto
-class ExtrairInformacoes(BaseModel):
-    """Extrair informações dos textos fornecidos"""
-    data: str = Field (description="data em que o o evento ocorreu")
-    contatos : str = Field(
-        description="nome das pessoas contidas no texto", 
-        examples=[
-            ("Me chamo Rafael e falei com o Junior sobre assunto XYZ.", "Rafael, Junior"), 
-            ("Me chamo Alfredo e falei com o Severino sobre assunto a posse dos Eua.", "Alfredo, Severino")
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def receive_message():
+    try:
+        body_corp = request.data  
+        body_str = body_corp.decode("utf-8")
+        print("Mensagem recebida:", body_str)  
+
+        class ExtraiInformacoes(BaseModel):
+            """Extrair informações dos textos fornecidos"""
+            data: str = Field (description="data em que o o evento ocorreu")
+            contatos : str = Field(
+                description="nome das pessoas contidas no texto", 
+                examples=[
+                    ("Me chamo Rafael e falei com o Junior sobre assunto XYZ.", "Rafael, Junior"), 
+                    ("Me chamo Alfredo e falei com o Severino sobre assunto a posse dos Eua.", "Alfredo, Severino")
+                ])
+            cargo: str = Field(description="cargo dos contatos mencionados")
+            organizacoes : str = Field(description="organizaçao dos contatos mencionados")
+            jurisdicoes : str = Field(
+                description="jurisdições mencionadas",
+                examples=[
+                    ("Jane Doe is a Senior Regulatory Advisor at the Financial Conduct Authority (FCA) in the UK. I don't have her email or phone number at the moment. ", "UK")
+                ])
+            representantes : str = Field(description="representantes dos contatos mencionados")
+            assunto : str = Field(description="assunto do texto, deve ser 'politica', 'economia' ou 'justica'.")
+            resumo : str = Field(description="resumo do texto, deve ser uma breve descrição do evento, com no máximo 100 caracteres.")
+            acoes_acompanhamento : str = Field(description="acoes de acompanhamento do texto.")
+            sentimento : str = Field(description="sentimento expresso pelo individuo, deve ser 'positivo', 'negativo' ou 'neutro'.")
+
+
+        @tool(args_schema=ExtraiInformacoes)
+        def extrutura_informacao(
+                data: str, 
+                contatos : str, 
+                cargo: str, 
+                organizacoes: str, 
+                jurisdicoes: str, 
+                representantes: str, 
+                assunto: str, 
+                resumo: str, 
+                acoes_acompanhamento: str, 
+                sentimento: str ):
+            
+            """Extrutura as informações do texto"""
+            return data, contatos, cargo, organizacoes, jurisdicoes, representantes, assunto, resumo, acoes_acompanhamento, sentimento
+
+        toolls = [extrutura_informacao]
+        toolls_json = [convert_to_openai_function(tooll) for tooll in toolls]
+
+        memory = ConversationBufferMemory(
+            return_messages=True,
+            memory_key="chat_history"
+        )
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", f"Você é um assistente juridico que extrai informações do texto fornecido. Para referencia a data atual é {data_atual}"),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad")
         ])
-    cargo: str = Field(description="cargo dos contatos mencionados")
-    organizacoes : str = Field(description="organizaçao dos contatos mencionados")
-    jurisdicoes : str = Field(
-        description="jurisdições mencionadas",
-        examples=[
-            ("Jane Doe is a Senior Regulatory Advisor at the Financial Conduct Authority (FCA) in the UK. I don't have her email or phone number at the moment. ", "UK")
-        ])
-    representantes : str = Field(description="representantes dos contatos mencionados")
-    assunto : str = Field(description="assunto do texto, deve ser 'politica', 'economia' ou 'justica'.")
-    resumo : str = Field(description="resumo do texto, deve ser uma breve descrição do evento, com no máximo 100 caracteres.")
-    acoes_acompanhamento : str = Field(description="acoes de acompanhamento do texto.")
-    sentimento : str = Field(description="sentimento expresso pelo individuo, deve ser 'positivo', 'negativo' ou 'neutro'.")
 
 
-
-#Prompt do agente
-prompt = ChatPromptTemplate.from_messages([
-        ("""system", "Você é um assistente juridico que identifica quem é o sujeito que fala, com quem ele conversou, qual as entidades 
-        mencionada no texto e qual o sentimento da conversa. Seja objetivo, sem papo furado. Caso o usuario não forneça alguma destas informações, pergunte"""),
-        ("user", "{input}")
-    ])
+        pass_through = RunnablePassthrough.assign(
+            agent_scratchpad=lambda x: format_to_openai_function_messages(x["intermediate_steps"])
+        )
+        agent_chain = pass_through | prompt | chat.bind(functions=toolls_json) | OpenAIFunctionsAgentOutputParser()
 
 
-#Criando a chain e convertendo a ferramemnta
-chamada_tool = convert_to_openai_function(ExtrairInformacoes)
-chain = (prompt | chat.bind_functions([chamada_tool], function_call={"name":"ExtrairInformacoes"}) | JsonOutputFunctionsParser())
+        def run_agent(input):
+            passos_intermediarios = []
+            while True:
+                resposta = agent_chain.invoke({
+                    "input": input,
+                    "agent_scratchpad": format_to_openai_function_messages(passos_intermediarios)
+                })
+                if isinstance(resposta, AgentFinish):
+                    return resposta
+                observacao = toolls[resposta.tool].run(resposta.tool_input)
+                passos_intermediarios.append((resposta, observacao))
 
-#invocando o modelo
-resposta = chain.invoke({"input": texto})
-print(f"\n\n{resposta}\n\n")
+
+        agent_executor = AgentExecutor(
+            agent=agent_chain,
+            memory=memory,
+            tools=toolls,
+            verbose=True,
+        )
+
+        resposta = agent_executor.invoke({"input": body_str})
+        resposta_final = resposta["output"]
+
+        return jsonify({"Status": resposta_final})
+    
+
+    except Exception as e:
+        return jsonify({"Erro": f"Falha ao processar JSON: {str(e)}"}), 500
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
